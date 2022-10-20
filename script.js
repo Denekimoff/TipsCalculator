55c2badda74978ea84e8ca314c35cd71369cabf3
Ekimov  Denis <ekimoffdenis@gmail.com>
2022-10-21 01:31:32 +0300

    add events

--- script.js
+++ script.js
@@ -1,40 +1,60 @@
 //* Обьявление всех переменных
-  // Поле ввода итоговой суммы чека
+// Поле ввода итоговой суммы чека
 const totalPriceElement = document.querySelector('.app__bill-total')
 
-  // Поле ввода количества человек за столом
+// Поле ввода количества человек за столом
 const numberPeopleElement = document.querySelector('.app__people-total')
 
-  // Псевдомассив кнопок процентов чаевых
+// Псевдомассив кнопок процентов чаевых
 const customTipsElements = document.querySelectorAll('.app__tip-choice')
 
-  // Итоговая сумма чаевых
+// Итоговая сумма чаевых
 const totalTipsElement = document.querySelector('.app__view-total span')
 
-  // Итоговая сумма чаевых на человека
+// Итоговая сумма чаевых на человека
 const tipsOnPeopleElement = document.querySelector('.app__view-amount span')
 
-  // Кнопка сброса данных
+// Кнопка сброса данных
 const resetBtnElement = document.querySelector('.app__reset')
 
 //* Алгоритмы событий
-  // Ограничение количества вводимых данных
-function handleMaxLengthPeople () {
+// Ограничение количества вводимых данных
+function handleMaxLengthPeople() {
   if (numberPeopleElement.value.length >= 2) {
     numberPeopleElement.value = numberPeopleElement.value.slice(0, 2)
     return
   }
 }
 
-function hadleToggleTips (event) {
+// Удаление активного класса
+function deleteClass() {
+  const arrtips = [...customTipsElements]
+  arrtips.forEach(item => item.classList.remove('--active'))
+}
+
+// Переключатель класса
+function hadleToggleTips(event) {
   const target = event.target
-  console.log('click')
   if (!target.closest('.app__tip-choice')) return
+  deleteClass()
   target.classList.toggle('--active')
-  console.log('clacccck')
+}
+
+// Подсчёт суммы чаевых
+let percent = 0.1
+function calcTips () {
+  totalTipsElement.textContent = totalPriceElement.value * percent
+}
+
+// Сброс полей ввода
+function resetForm(event) {
+  if (event.target !== resetBtnElement) return
+  totalPriceElement.value = ''
+  numberPeopleElement.value = ''
 }
 
 //* Обработчики событий
-  // Изменение поля ввода количества человек
+// Изменение поля ввода количества человек
 numberPeopleElement.addEventListener('input', handleMaxLengthPeople)
 window.document.addEventListener('click', hadleToggleTips)
+window.document.addEventListener('click', resetForm)
