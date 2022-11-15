c8a53bd0ef9057c99ef18487bf9a4c1e62998e55
Ekimov  Denis <ekimoffdenis@gmail.com>
2022-11-15 14:48:01 +0300

    feat: add eventListeners

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const totalPriceElement = document.querySelector('.app__bill-total')
 const numberPeopleElement = document.querySelector('.app__people-total')
 
 // Псевдомассив кнопок процентов чаевых
+const containerCustomTipsElement = document.querySelector('app__tip-all')
 const customTipsElements = document.querySelectorAll('.app__tip-choice')
 
 // Итоговая сумма чаевых
@@ -17,12 +18,24 @@ const tipsOnPeopleElement = document.querySelector('.app__view-amount span')
 // Кнопка сброса данных
 const resetBtnElement = document.querySelector('.app__reset')
 
+// Набор цифр, которые можно вводить
+const validNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
+
 //* Алгоритмы событий
-// Ограничение количества вводимых данных
-function handleMaxLengthPeople() {
+// Ограничение количества вводимых данных человек
+function handlerMaxLengthPeople () {
   if (numberPeopleElement.value.length >= 2) {
     numberPeopleElement.value = numberPeopleElement.value.slice(0, 2)
     return
+  }
+}
+
+// Ограничение ввода суммы чека
+function handlerInputTotalPrice () {
+  const value = totalPriceElement.value
+  if (!validNumbers.includes(value[value.length - 1])) {
+    console.log(value)
+
   }
 }
 
@@ -33,7 +46,7 @@ function deleteClass() {
 }
 
 // Переключатель класса
-function hadleToggleTips(event) {
+function handlerToggleTips(event) {
   const target = event.target
   if (!target.closest('.app__tip-choice')) return
   deleteClass()
@@ -41,9 +54,20 @@ function hadleToggleTips(event) {
 }
 
 // Подсчёт суммы чаевых
-let percent = 0.1
-function calcTips () {
-  totalTipsElement.textContent = totalPriceElement.value * percent
+let percent = 10
+function calcTips() {
+  const arrtips = [...customTipsElements]
+  const activeTips = arrtips.filter(tip => tip.className.includes('--active'))
+  const valueTips = +activeTips[0].value.slice(0, 2)
+  const tips = totalPriceElement.value / 100 * valueTips
+  totalTipsElement.textContent = tips.toFixed(3)
+
+  calcTipsOnPerson( +totalTipsElement.textContent )
+}
+
+// Подсчёт суммы чаевых на человека
+function calcTipsOnPerson (price) {
+ tipsOnPeopleElement.textContent = (price / numberPeopleElement.value).toFixed(3)
 }
 
 // Сброс полей ввода
@@ -51,10 +75,17 @@ function resetForm(event) {
   if (event.target !== resetBtnElement) return
   totalPriceElement.value = ''
   numberPeopleElement.value = ''
+  totalTipsElement.textContent = '0'
+  tipsOnPeopleElement.textContent = '0'
+  deleteClass()
 }
 
 //* Обработчики событий
-// Изменение поля ввода количества человек
-numberPeopleElement.addEventListener('input', handleMaxLengthPeople)
-window.document.addEventListener('click', hadleToggleTips)
+totalPriceElement.addEventListener('input', handlerInputTotalPrice)
+totalPriceElement.addEventListener('input', calcTips)
+numberPeopleElement.addEventListener('input', handlerMaxLengthPeople)
+numberPeopleElement.addEventListener('input', calcTips)
+window.document.addEventListener('click', handlerToggleTips)
 window.document.addEventListener('click', resetForm)
+containerCustomTipsElement.addEventListener('click', calcTips)
+containerCustomTipsElement.addEventListener('input', calcTips)
